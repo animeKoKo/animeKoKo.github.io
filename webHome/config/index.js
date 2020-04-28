cc6d88cc8274ba3032cac6a61a36698409168295
sq.momoko.soulCoder <1094388023@qq.com>
2020-04-28 16:12:31 +0800

    new

--- webHome/config/index.js
+++ webHome/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../blog'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
